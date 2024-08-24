46295ac2bed661ef20cd60331619d91e362025ec
danielr <daniel.ratmiroff@sdui.de>
2024-08-24 20:55:29 +0200

    add config

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	GroqAPIKey string
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("$HOME/.terminaider")
+
+	viper.SetDefault("groq_api_key", "")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			fmt.Println("Config file not found. Using default values.")
+		} else {
+			return nil, fmt.Errorf("error reading config file: %w", err)
+		}
+	}
+
+	// Check for environment variable
+	if envAPIKey := os.Getenv("GROQ_API_KEY"); envAPIKey != "" {
+		viper.Set("groq_api_key", envAPIKey)
+	}
+
+	var config Config
+	err := viper.Unmarshal(&config)
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode config into struct: %w", err)
+	}
+
+	return &config, nil
+}
